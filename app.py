b188836a17333a56b9254d3e520bbd0a87a0fa16
vitor gomes <vitor.araujo.derreteman4@gmail.com>
2026-03-31 10:57:45 -0300

    estabilishing database sqlalchemy config

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__) #instanciando flask
+app.config['SECRET_KEY'] = "your_secret_key"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 
-app = Flask(__name__)
-
+db = SQLAlchemy(app) #instanciando Database com aplicação flask
 
 @app.route("/Ola", methods=['GET'])
 def ola():
