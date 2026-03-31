9bab847cb92552e790dfbba5bddb3fac9929c867
vitor gomes <vitor.araujo.derreteman4@gmail.com>
2026-03-31 11:27:49 -0300

    movendo criação do database para database.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, request, jsonify, render_template
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__) #instanciando flask
 app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 
-db = SQLAlchemy(app) #instanciando Database com aplicação flask
+#Session <- conexão ativa
 
 @app.route("/Ola", methods=['GET'])
 def ola():
